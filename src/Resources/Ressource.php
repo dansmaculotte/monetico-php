44af33bb38659f74d0780e7f400df60a20491226
Gaël Reyrol <me@gaelreyrol.com>
2019-09-11 16:24:48 +0200

    fix: return if value is not defined in setParameter method

--- src/Resources/Ressource.php
+++ src/Resources/Ressource.php
@@ -46,10 +46,14 @@ class Ressource
      * @param mixed $value
      * @throws Exception
      */
-    public function setParameter(string $name, $value): void
+    public function setParameter(string $name, $value = null): void
     {
         if (!in_array($name, $this->keys)) {
             throw Exception::invalidResourceParameter($name);
+        }
+
+        if (!$value) {
+            return ;
         }
 
         $this->parameters[$name] = $value;
@@ -62,11 +66,7 @@ class Ressource
     public function setParameters(array $fields): void
     {
         foreach ($fields as $fieldKey => $fieldValue) {
-            if (!in_array($fieldKey, $this->keys)) {
-                throw Exception::invalidResourceParameter($fieldKey);
-            }
-
-            $this->parameters[$fieldKey] = $fieldValue;
+            $this->setParameter($fieldKey, $fieldValue);
         }
     }
 
